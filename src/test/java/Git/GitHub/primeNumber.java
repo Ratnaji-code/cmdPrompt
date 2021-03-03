f07a052e95ebfc234b61574d8303731ba99040b6
Ratnaji-code <ratnaji.gidda@gmail.com>
2021-03-03 15:39:50 +0530

    added a primenumber program

---
+++ src/test/java/Git/GitHub/primeNumber.java
@@ -0,0 +1,25 @@
+package Git.GitHub;
+
+import java.util.Scanner;
+
+public class primeNumber {
+
+	public static void main(String[] args) {
+	
+     int n = 7;
+    boolean flag= true;
+    int m=n/2;
+    for(int i=2;i<m;i++) {
+    	if(n%i==0) {
+    		System.out.println(i+": is not a prime number");
+    		flag=false;
+    		break;
+    	}//if
+    }//for
+    if(flag==true){
+		System.out.println(n+": is  a prime number");
+
+	}
+}
+	
+}
